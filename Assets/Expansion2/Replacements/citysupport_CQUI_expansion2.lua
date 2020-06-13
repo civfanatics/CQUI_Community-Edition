44183fa2012bd7a279f4c08a9b943619b8a3d551
the-m4a <michaelfourre@hotmail.com>
2020-06-13 00:46:19 -0700

    Reduce Replacement Files Work

---
+++ Assets/Expansion2/Replacements/citysupport_CQUI_expansion2.lua
@@ -0,0 +1,7 @@
+-- ===========================================================================
+-- Base File
+-- ===========================================================================
+-- Note: CitySupport.Lua is a complete replacement in the Expansion2 from Firaxis
+include("CitySupport");
+
+include("citysupport_CQUI.lua");
