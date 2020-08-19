dda175b4a55d4f2c95cc827572ed31f9c6e035bb
the-m4a <michaelfourre@hotmail.com>
2020-08-18 23:26:08 -0700

    added the researchchooser other files

---
+++ Assets/Expansion1/Replacements/researchchooser_CQUI_expansion1.lua
@@ -0,0 +1,9 @@
+-- ===========================================================================
+-- Base File
+-- ===========================================================================
+--TEMP
+print("****** researchchooser_CQUI_expansion1 LOADED")
+
+include("ResearchChooser_Expansion1");
+
+include("researchchooser_CQUI.lua");
