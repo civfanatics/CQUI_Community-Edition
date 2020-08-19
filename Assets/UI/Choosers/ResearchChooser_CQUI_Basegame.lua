eeefdae45aa3e4f29d9ebdea3162f1272661e2e3
the-m4a <the-m4a@users.noreply.github.com>
2020-08-18 23:59:13 -0700

    Update ResearchChooser_CQUI_Basegame.lua

--- Assets/UI/Choosers/ResearchChooser_CQUI_Basegame.lua
+++ Assets/UI/Choosers/ResearchChooser_CQUI_Basegame.lua
@@ -4,4 +4,4 @@
 -- Note: There is no ReseachChooser_Expansion2, as ResearchChooser_Expansion1 (one) is the file that is bundled with both the Rise and Fall and Gathering Storm expansions
 include("ResearchChooser");
 
-include("researchchooser_CQUI.lua");
+include("ResearchChooser_CQUI.lua");
