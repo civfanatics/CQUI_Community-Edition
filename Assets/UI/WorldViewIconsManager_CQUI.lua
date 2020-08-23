2ef732a03f57536d03fcf3e1a1cb733820127b01
the-m4a <michaelfourre@hotmail.com>
2020-08-22 23:15:18 -0700

    Squashed commit of the following:
    commit fd87bc653f9f2e0d5f77dc8cf5a525b70be806f3
    Author: the-m4a <michaelfourre@hotmail.com>
    Date:   Wed Aug 19 22:26:52 2020 -0700
    
        Renamed to accepted casing scheme
    
    commit 1f01d62f245ea96c6b036e146456b7025b166e99
    Author: the-m4a <michaelfourre@hotmail.com>
    Date:   Wed Aug 19 22:22:19 2020 -0700
    
        Cleanup for PR

--- Assets/UI/WorldViewIconsManager_CQUI.lua
+++ Assets/UI/WorldViewIconsManager_CQUI.lua
@@ -2,33 +2,38 @@
 -- Base File
 -- ===========================================================================
 include("WorldViewIconsManager");
+include("CQUICommon.lua");
 
 -- ===========================================================================
 -- Cached Base Functions
 -- ===========================================================================
-BASE_SetResourceIcon = SetResourceIcon;
+BASE_CQUI_SetResourceIcon = SetResourceIcon;
 
 -- ===========================================================================
 -- CQUI Members
 -- ===========================================================================
-local CQUI_ResourceIconStyle = 1;
+local CQUI_RESOURCEICONSTYLE_SOLID = 0;
+local CQUI_RESOURCEICONSTYLE_TRANSPARENT = 1;
+local CQUI_RESOURCEICONSTYLE_HIDDEN = 2;
 
+local CQUI_ResourceIconStyle = CQUI_RESOURCEICONSTYLE_TRANSPARENT;
+
+-- ===========================================================================
 function CQUI_GetSettingsValues()
     CQUI_ResourceIconStyle = GameConfiguration.GetValue("CQUI_ResourceDimmingStyle");
     if CQUI_ResourceIconStyle == nil then
-        print("CQUI_ResourceIconStyle is nil!  Using default value.");
-        CQUI_ResourceIconStyle = 1;
+        print("CQUI_ResourceIconStyle is nil!  Using default value (CQUI_RESOURCEICONSTYLE_TRANSPARENT).");
+        CQUI_ResourceIconStyle = CQUI_RESOURCEICONSTYLE_TRANSPARENT;
     end
 end
 
+-- ===========================================================================
 function CQUI_OnIconStyleSettingsUpdate()
     CQUI_GetSettingsValues();
     Rebuild();
 end
 
-LuaEvents.CQUI_SettingsUpdate.Add( CQUI_OnIconStyleSettingsUpdate );
-LuaEvents.CQUI_SettingsInitialized.Add(CQUI_GetSettingsValues);
-
+-- ===========================================================================
 function CQUI_IsResourceOptimalImproved(resourceInfo, pPlot)
     if table.count(resourceInfo.ImprovementCollection) > 0 then
         for _, improvement in ipairs(resourceInfo.ImprovementCollection) do
@@ -53,8 +58,12 @@ end
 --  CQUI modified SetResourceIcon functiton : Improved resource icon dimming/hiding
 -- ===========================================================================
 function SetResourceIcon( pInstance:table, pPlot, type, state)
-    BASE_SetResourceIcon(pInstance, pPlot, type, state);
+    BASE_CQUI_SetResourceIcon(pInstance, pPlot, type, state);
+    CQUI_SetResourceIconStyle(pInstance, pPlot, type, state, CQUI_ResourceIconStyle);
+end
 
+-- ===========================================================================
+function CQUI_SetResourceIconStyle(pInstance, pPlot, type, state, iconStyle)
     local resourceInfo = GameInfo.Resources[type];
     if (pPlot and resourceInfo ~= nil) then
         local resourceType:string = resourceInfo.ResourceType;
@@ -66,15 +75,15 @@ function SetResourceIcon( pInstance:table, pPlot, type, state)
         local textureOffsetX, textureOffsetY, textureSheet = IconManager:FindIconAtlas(iconName, 256);
         if (textureSheet ~= nil) then
             if (pPlot:GetOwner() == Game.GetLocalPlayer()) then --Only affects plots we own
-                if (CQUI_ResourceIconStyle == 0) then
+                if (iconStyle == CQUI_RESOURCEICONSTYLE_SOLID) then
                     pInstance.ResourceIcon:SetColor(1,1,1,1);
-                elseif (CQUI_ResourceIconStyle == 1) then
+                elseif (iconStyle == CQUI_RESOURCEICONSTYLE_TRANSPARENT) then
                     if (CQUI_IsResourceOptimalImproved(resourceInfo, pPlot)) then
                         pInstance.ResourceIcon:SetColor(1,1,1,0.5);
                     else
                         pInstance.ResourceIcon:SetColor(nil);
                     end
-                elseif (CQUI_ResourceIconStyle == 2) then
+                elseif (iconStyle == CQUI_RESOURCEICONSTYLE_HIDDEN) then
                     if (CQUI_IsResourceOptimalImproved(resourceInfo, pPlot)) then
                         pInstance.ResourceIcon:SetColor(1,1,1,0);
                     else
@@ -93,3 +102,38 @@ end
 function AddImprovementRecommendationsForCity( pCity:table, pSelectedUnit:table )
     return;
 end
+
+-- ===========================================================================
+function CQUI_OnImprovementAdded(locationX, locationY)
+    CQUI_OnImprovementChanged(locationX, locationY, true);
+end
+
+-- ===========================================================================
+function CQUI_OnImprovementRemoved(locationX, locationY)
+    CQUI_OnImprovementChanged(locationX, locationY, false);
+end
+
+-- ===========================================================================
+function CQUI_OnImprovementChanged(locationX, locationY, isAdded)
+    print_debug("CQUI_OnImprovementChanged ENTRY. x:"..locationX.."  y:"..locationY);
+
+    local plot = Map.GetPlot(locationX, locationY);
+    local resourceType = plot:GetResourceType();
+    local plotIndex = Map.GetPlotIndex(locationX, locationY);
+    local pInstance = GetInstanceAt(plotIndex);
+    local iconStyle = CQUI_ResourceIconStyle;
+    if (isAdded == false) then
+        iconStyle = CQUI_RESOURCEICONSTYLE_SOLID;
+    end
+
+    CQUI_SetResourceIconStyle(pInstance, plot, resourceType, RevealedState.VISIBLE, iconStyle);
+end
+
+-- ===========================================================================
+function Initialize()
+    LuaEvents.CQUI_SettingsUpdate.Add( CQUI_OnIconStyleSettingsUpdate );
+    LuaEvents.CQUI_SettingsInitialized.Add(CQUI_GetSettingsValues);
+    Events.ImprovementAddedToMap.Add(CQUI_OnImprovementAdded);
+    Events.ImprovementRemovedFromMap.Add(CQUI_OnImprovementRemoved);
+end
+Initialize();
