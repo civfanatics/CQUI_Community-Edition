0305f51a003c1695b822ee21c22c943ff76131ca
the-m4a <michaelfourre@hotmail.com>
2020-08-30 01:53:14 -0700

    Remove CQUI CitySupport files

--- Assets/Expansion2/Replacements/citysupport_CQUI_expansion2.lua
+++
@@ -1,7 +0,0 @@
--- ===========================================================================
--- Base File
--- ===========================================================================
--- Note: CitySupport.Lua is a complete replacement in the Expansion2 from Firaxis
-include("CitySupport");
-
-include("citysupport_CQUI.lua");
