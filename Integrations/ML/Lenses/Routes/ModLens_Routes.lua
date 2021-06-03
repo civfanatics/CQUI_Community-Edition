f857a2babcb4bf16eafcd4a5fdffb7631ffa6da9
the-m4a <michaelfourre@hotmail.com>
2021-06-03 00:02:22 -0700

    Auto-apply when Englneer Selected

--- Integrations/ML/Lenses/Routes/ModLens_Routes.lua
+++ Integrations/ML/Lenses/Routes/ModLens_Routes.lua
@@ -7,12 +7,11 @@ local m_LensSettings = {
     ["COLOR_ROUTES_LENS_RAIL"] = { ConfiguredColor = GetLensColorFromSettings("COLOR_ROUTES_LENS_RAIL"),  KeyLabel = "LOC_HUD_ROUTES_LENS_RAIL" }
 }
 
--- ===========================================================================
--- Routes (Road and Rail) Lens Support
--- ===========================================================================
+-- Should the archaeologist lens auto apply, when a archaeologist is selected.
+local AUTO_APPLY_ENGINEER_LENS:boolean = false
 
 -- ===========================================================================
--- Exported functions
+-- Routes (Road and Rail) Lens Support
 -- ===========================================================================
 
 local function OnGetColorPlotTable()
@@ -45,12 +44,13 @@ local function OnGetColorPlotTable()
     return colorPlot
 end
 
---[[
+-- ===========================================================================
 local function ShowRoutesLens()
     LuaEvents.MinimapPanel_SetActiveModLens(LENS_NAME)
     UILens.ToggleLayerOn(ML_LENS_LAYER)
 end
 
+-- ===========================================================================
 local function ClearRoutesLens()
     if UILens.IsLayerOn(ML_LENS_LAYER) then
         UILens.ToggleLayerOff(ML_LENS_LAYER)
@@ -58,13 +58,59 @@ local function ClearRoutesLens()
     LuaEvents.MinimapPanel_SetActiveModLens("NONE")
 end
 
-local function OnInitialize()
-    -- Nothing to do
+-- ===========================================================================
+local function OnUnitSelectionChanged( playerID:number, unitID:number, hexI:number, hexJ:number, hexK:number, bSelected:boolean, bEditable:boolean )
+    if playerID == Game.GetLocalPlayer() then
+        local unitType = GetUnitTypeFromIDs(playerID, unitID)
+        if unitType then
+            if bSelected then
+                if unitType == "UNIT_MILITARY_ENGINEER" and AUTO_APPLY_ENGINEER_LENS then
+                    ShowRoutesLens()
+                end
+            -- Deselection
+            else
+                if unitType == "UNIT_MILITARY_ENGINEER" and AUTO_APPLY_ENGINEER_LENS then
+                    ClearRoutesLens()
+                end
+            end
+        end
+    end
 end
-]]
+
+-- ===========================================================================
+local function OnUnitRemovedFromMap( playerID: number, unitID : number )
+    local localPlayer = Game.GetLocalPlayer()
+    local lens = {}
+    LuaEvents.MinimapPanel_GetActiveModLens(lens)
+    if playerID == localPlayer then
+        if lens[1] == LENS_NAME and AUTO_APPLY_ENGINEER_LENS then
+            ClearRoutesLens()
+        end
+    end
+end
+
+-- ===========================================================================
+-- For modded lens during multiplayer. Might need to test this further
+function OnUnitCaptured( currentUnitOwner, unit, owningPlayer, capturingPlayer )
+    local localPlayer = Game.GetLocalPlayer()
+    if owningPlayer == localPlayer then
+        local unitType = GetUnitTypeFromIDs(owningPlayer, unitID)
+        if unitType and unitType == "UNIT_MILITARY_ENGINEER" and AUTO_APPLY_ENGINEER_LENS then
+            ClearRoutesLens()
+        end
+    end
+end
+
+-- ===========================================================================
+local function OnInitialize()
+    Events.UnitSelectionChanged.Add( OnUnitSelectionChanged )
+    Events.UnitRemovedFromMap.Add( OnUnitRemovedFromMap )
+    Events.UnitCaptured.Add( OnUnitCaptured )
+end
 
 -- ===========================================================================
 local function CQUI_OnSettingsInitialized()
+    AUTO_APPLY_ENGINEER_LENS = GameConfiguration.GetValue("CQUI_AutoapplyEngineerLens");
     UpdateLensConfiguredColors(m_LensSettings, g_ModLensModalPanel, LENS_NAME);
 end
 
@@ -76,7 +122,7 @@ end
 local RoutesLensEntry = {
     LensButtonText = "LOC_HUD_ROUTES_LENS",
     LensButtonTooltip = "LOC_HUD_ROUTES_LENS_TOOLTIP",
-    Initialize = nil,
+    Initialize = OnInitialize,
     GetColorPlotTable = OnGetColorPlotTable
 }
 
